4f1c4d89355af31c682911db8725dff37c31306a
alvmiller <134068320+alvmiller@users.noreply.github.com>
2025-08-26 23:17:09 +0300

    Create two_sum.py

---
+++ algs/leetcode/python/two_sum.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hm = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in hm:
+                return [i, hm[diff]]
+            hm[nums[i]] = i
